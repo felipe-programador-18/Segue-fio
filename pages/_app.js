ea9a1756f6825dde93e260502f5bf658454409e3
Felipe <machadofelipe2016@outlook.com>
2022-06-12 14:52:21 -0300

    c0-get started new project call wire follow

---
+++ pages/_app.js
@@ -0,0 +1,7 @@
+import '../styles/globals.css'
+// this it works tailwindcss
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />
+}
+
+export default MyApp
---
+++ pages/seguefio.js
@@ -0,0 +1,20 @@
+
+
+const followlaine = () => {
+    return(<>
+    <img  className="align-items-center" src="logo-segue-fio.png" alt="seguefio" />
+    <p>....</p>
+       <div className="flex flex-row bg-gray-400 ">
+         <div>
+          <textarea  className="w-full h-screen" ></textarea>
+         </div>
+         
+         <div>
+          <h2>Twits</h2>
+         </div>
+       
+       </div> 
+    </>)
+}
+
+export default followlaine
