7a3daf5df45496e8c9b72fda59c024a54cec0fea
Felipe <machadofelipe2016@outlook.com>
2022-06-13 14:51:38 -0300

    c3-adding some modify and started create new code`

--- pages/seguefio.js
+++ pages/seguefio.js
@@ -1,17 +1,33 @@
+import { useForm } from "react-hook-form"
 
 
 const followlaine = () => {
+  const {register, watch} = useForm()  
+  
+  const content = watch("content")
+  const twits = content?.split("\n\n\n")
+    
+    
     return(<>
     <img  className="align-items-center" src="logo-segue-fio.png" alt="seguefio" />
     <p>....</p>
-       <div className="flex flex-row bg-gray-300 ">
-         <div>
-          <textarea  className="w-full h-screen" ></textarea>
+       <div className="flex flex-row p-6">
+        
+         <div className="flex-1" >
+          <h2>Insights: </h2>
+          <textarea {...register("content")}  className="w-full h-screen" ></textarea>
          </div>
          
-         <div>
-          <h2>Twits</h2>
-         </div>
+         <div className="flex-1 p-2">
+            
+            <h2>Twits:</h2>
+           
+            { twits?.map( (twit) => (<div className=" shadow border border-red-600 rounded-lg p-4">
+            Twits {twit} 
+              </div>
+             ))}
+          
+          </div>
        
        </div> 
     </>)
