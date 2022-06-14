c74e1200aea912dfab0faf9ae845c1b017f49589
Felipe <machadofelipe2016@outlook.com>
2022-06-14 14:09:05 -0300

    c15- adding map and filter in variable to make another stratagy

--- pages/seguefio.js
+++ pages/seguefio.js
@@ -3,15 +3,20 @@ import Twits from "../Components/twis"
 
 const followlaine = () => {
   const {register, watch} = useForm()  
-  
+  const showCounter= watch("showCounter") || false
+
   const content = watch("content") || ""
-  const twits = content?.split("\n\n\n")
+  const twits = content?.split("\n\n\n").map(twit => twit.trim()).filter( (twit) => twit )
     
     
     return(<div className="max-w-6xl m-auto" > 
     <img  className="" src="logo-segue-fio.png" alt="seguefio" />
-    <p>....</p>
-       <div className="flex flex-row p-6">
+      
+       <p className="px-8"> <label> <input className="ml-8 m-2" type='checkbox' {...register('showCounter')} />Mostrar Contador</label>  </p>
+       
+       
+       
+       <div className="flex flex-row px-6 py-2">
         
          <div className="flex-1 m-4" >
           <h2>Insights: </h2>
@@ -23,7 +28,12 @@ const followlaine = () => {
             <h2>Twits:</h2>
            
             { twits?.map( (twit, index) =>( 
-             <Twits twit={twit} key={index}  />
+             <Twits twit={twit} 
+             key={index}
+             showCounter={showCounter}
+             current={index+1}
+             total={twits.length}
+             />
              ))}
           
           </div>
